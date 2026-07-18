a2448dba626d5bcc346ab891e488d9ad51723344
rachel72772 <rachelcortesutrera@gmail.com>
2026-07-18 19:22:46 +0200

    Add Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Sustituye estos valores por los de Firebase Console > Configuración del proyecto > Tu app web.
+export const firebaseConfig = {
+  apiKey: "REEMPLAZAR",
+  authDomain: "REEMPLAZAR.firebaseapp.com",
+  projectId: "REEMPLAZAR",
+  storageBucket: "REEMPLAZAR.appspot.com",
+  messagingSenderId: "REEMPLAZAR",
+  appId: "REEMPLAZAR"
+};
